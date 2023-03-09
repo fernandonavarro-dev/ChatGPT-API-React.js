27573df32309de5ae9aa2ae4da7201637eb9be91
Fernando Navarro <fernandonavarro@berkeley.edu>
2023-03-08 21:37:37 -0800

    ChatGPT API implemented and functional
    Async/await function for ChatGPT API is working and try/catch written to console.error(error)

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ import {
   TypingIndicator,
 } from '@chatscope/chat-ui-kit-react';
 
+const systemMessage = {
+  role: 'system',
+  content: "Explain all concepts like I'm a junior/mid-level web developer",
+};
+
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm ChatGPT! Ask me anything!",
@@ -29,14 +35,71 @@ function App() {
     const newMessages = [...messages, newMessage];
 
     setMessages(newMessages);
+
+    setTyping(true);
+
+    await processMessageToChatGPT(newMessages);
   };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = '';
+      if (messageObject.sender === 'ChatGPT') {
+        role = 'assistant';
+      } else {
+        role = 'user';
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    try {
+      const response = await fetch(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: 'Bearer ' + process.env.REACT_APP_OPENAI_API_KEY,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(apiRequestBody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data from the ChatGPT API');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setMessages([
+        ...chatMessages,
+        {
+          message: data.choices[0].message.content,
+          sender: 'ChatGPT',
+        },
+      ]);
+      setTyping(false);
+    } catch (error) {
+      console.error(error);
+      setTyping(false);
+    }
+  }
 
   return (
     <div className="App">
       <div style={{ position: 'relative', height: '800px', width: '700px' }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              typingIndicator={
+                typing ? <TypingIndicator content="ChatGPT is typing" /> : null
+              }
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />;
               })}
